08eb30133e99bbf1ef7a9f28576379895e7c8f8d
marcellinoknl <marcellinokn@gmail.com>
2023-07-03 08:24:54 +0800

    push data chart

--- database/seeders/MemberSeeder.php
+++ database/seeders/MemberSeeder.php
@@ -15,16 +15,30 @@ class MemberSeeder extends Seeder
     {
         Member::create([
             'department_id' => 1,
-            'position_id' => 1,
+            'position_id' => 2,
             'prodi_id' => 1,
-            'name' => 'Jhon',
-            'nim' => 'ifs19001',
-            'email' => 'jhon@gmail',
+            'name' => 'Jhon Videlis Simamora',
+            'nim' => '11S18023',
+            'email' => 'jhonvidelis@gmail.com',
             'phone' => '08123456789',
             'image' => 'java.png',
             'linkedin' => 'https://www.linkedin.com/in/',
             'instagram' => 'https://www.instagram.com/',
             'github' => 'https://github.com/',
-        ]);
+        ],
+        [
+            'department_id' => 1,
+            'position_id' => 1,
+            'prodi_id' => 3,
+            'name' => 'Daniel Simbolon',
+            'nim' => '14S20020',
+            'email' => 'simbolondaniel007@gmail.com',
+            'phone' => '081320585103',
+            'image' => 'java.png',
+            'linkedin' => 'https://www.linkedin.com/in/',
+            'instagram' => 'https://www.instagram.com/',
+            'github' => 'https://github.com/',
+        ]
+    );
     }
 }
